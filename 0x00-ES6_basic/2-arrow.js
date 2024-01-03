3c723d077ecf6e540d3e9cac04d4d2ec42816f5f
assouljawad <38533122+assouljawad@users.noreply.github.com>
2024-01-03 13:55:37 +0100

    Arrow Functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default function getNeighborhoodsList() {
-  /* eslint-disable no-invalid-this */
   this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
