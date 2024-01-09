625546a44344715ec293a4467c0091092ad1d6e9
assouljawad <38533122+assouljawad@users.noreply.github.com>
2024-01-09 11:19:01 +0100

    get list students - fix export function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,18 @@
-function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
-    },
-    {
-      id: 2,
-      firstName: "James",
-      location: "Columbia",
-    },
-    {
-      id: 3,
-      firstName: "Serena",
-      location: "San Francisco",
-    },
-  ];
-}
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    firstName: "James",
+    location: "Columbia",
+  },
+  {
+    id: 3,
+    firstName: "Serena",
+    location: "San Francisco",
+  },
+];
 export default getListStudents;
